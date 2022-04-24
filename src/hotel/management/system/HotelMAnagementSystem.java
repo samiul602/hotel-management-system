02fbb960852e1dea8a592a8a6be0442407ec0469
unknown <sami.alam602@gmail.com>
2022-04-25 02:59:34 +0600

    initial commit

---
+++ src/hotel/management/system/HotelMAnagementSystem.java
@@ -0,0 +1,17 @@
+
+package hotel.management.system;
+
+/**
+ *
+ * @author hp
+ */
+public class HotelMAnagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
